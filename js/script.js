0a4a68b978feed500e9bd8f71ae3c0414b0ef788
AlinaMytsa <act.mitsa@gmail.com>
2022-08-17 20:28:18 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const onePotatoWeight = 0.075;
-const pintsOfBorsch = 48;
+// const pintsOfBorsch = 48;
 const priceOnePoundOfPotato = 13;
 
 
-const getPotatoWeight  = () => {
+const getPotatoWeight  = (pintsOfBorsch = 48 ) => {
 
   const potatoForOnePint = onePotatoWeight * 4;
   const potatoFor48Pints = (pintsOfBorsch * potatoForOnePint);
   const priceOfAllPotato = potatoFor48Pints * priceOnePoundOfPotato;
 
   let a = Math.round(priceOfAllPotato);
-
+  return a;
 
 }
 
 getPotatoWeight();
+
